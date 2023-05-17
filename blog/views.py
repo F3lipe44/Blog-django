be1f6e14883bdf860136f274249db13ecf3a6097
fds <dorrego05@gmail.com>
2023-05-17 10:08:44 +0200

    segunda pag

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
-
+def shop(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/shop.html', {'posts': posts})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
